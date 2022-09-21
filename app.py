bb5432fb1941e118b0e991aa6e15dd41aab45f1f
Darshil Modi <45987056+darshil3011@users.noreply.github.com>
2022-09-21 14:32:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,17 @@ def run_model(input_text):
 
 if st.button('Submit'):
     run_model(text)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   #st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   #st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   #st.image("https://static.streamlit.io/examples/owl.jpg")
